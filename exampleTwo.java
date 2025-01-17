07262b62f70e294f7630f91090e9f08cd84a8088
shawngobrzy <117478227+shawngobrzy@users.noreply.github.com>
2025-01-16 21:46:14 -0500

    Update exampleTwo.java

--- exampleTwo.java
+++ exampleTwo.java
@@ -1,12 +1,12 @@
-class Main {
+public class MultiplyTwoNumbers {
 
-  public static void main(String[] args) {
-    
-    int first = 10;
-    int second = 20;
+    public static void main(String[] args) {
 
-    // add two numbers
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
-  }
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
 }
